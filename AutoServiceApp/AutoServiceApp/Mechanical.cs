8cccd0eebe462b97bd5548f215125a86660c1834
MadianB <madianbojan2004@gmail.com>
2025-01-12 19:27:41 +0200

    mecanic
    clasa mecanic

---
+++ AutoServiceApp/AutoServiceApp/Mechanical.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoServiceApp
+{
+    public class Mechanic : User
+    {
+        public Mechanic(string codUnic, string nume, string prenume, string email, string parola)
+            : base(codUnic, nume, prenume, email, parola) { }
+
+        public CerereRezolvare PreluareCerere()
+        {
+
+        }
+
+        public bool InvestigareProblema()
+        {
+
+        }
+
+        public void CreeazaCererePiese()
+        {
+
+        }
+
+        public void RezolvaProblema()
+        {
+
+        }
+    }
+}
