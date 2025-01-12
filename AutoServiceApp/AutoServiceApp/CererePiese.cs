02bc5d98832af0eafe0dea8ef7ba5a7c4a60b41a
Isidor Michael <mikybanu@gmail.com>
2025-01-12 19:03:25 +0200

    Clasa Cerere Piese

---
+++ AutoServiceApp/AutoServiceApp/CererePiese.cs
@@ -0,0 +1,25 @@
+﻿namespace AutoServiceApp
+{
+    public enum PartOrderStatus
+    {   InAsteptare,
+        InProcesare,
+        InPreluare,
+        Finalizat
+    }
+
+    public class CererePiese
+    {
+        public int Avb { get; set; }
+        public string NumeMecanic { get; set; }
+        public string DetaliiPiese { get; set; }
+        public PartOrderStatus Status { get; set; }
+
+        public CererePiese(int avb, string numeMecanic, string detaliiPiese, PartOrderStatus status)
+        {
+            Avb = avb;
+            NumeMecanic = numeMecanic;
+            DetaliiPiese = detaliiPiese;
+            Status = status;
+        }
+    }
+}
