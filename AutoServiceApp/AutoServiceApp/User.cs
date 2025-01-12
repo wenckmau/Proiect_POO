d2785fba8db641f495ea4038b83947875a5a5cb2
Isidor Michael <mikybanu@gmail.com>
2025-01-12 18:38:45 +0200

    Clasa User

---
+++ AutoServiceApp/AutoServiceApp/User.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoServiceApp
+{
+    public abstract class User
+    {
+        public string CodUnic { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Email { get; set; }
+        public string Parola { get; set; }
+
+        protected User(string codUnic, string nume, string prenume, string email, string parola)
+        {
+            CodUnic = codUnic;
+            Nume = nume;
+            Prenume = prenume;
+            Email = email;
+            Parola = parola;
+        }
+    }
+}
