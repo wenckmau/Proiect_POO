6b16644d54f49d8db84e392c237709b91340a322
luci12332 <106401960+luci12332@users.noreply.github.com>
2025-01-12 19:40:20 +0200

    CerereRezolvare

--- AutoServiceApp/AutoServiceApp/obj/Debug/net8.0/AutoServiceApp.AssemblyInfo.cs
+++ AutoServiceApp/AutoServiceApp/obj/Debug/net8.0/AutoServiceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoServiceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9c50912a4acf38fc214553c24b04dc24549fd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a22da2825461355e0263a77522e5ef0d9d28a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoServiceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoServiceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
