d95327ad1b661b993b79ee05004c2187d1f4b873
Isidor Michael <mikybanu@gmail.com>
2025-01-13 02:15:01 +0200

    Implementare State Manager

---
+++ AutoServiceApp/AutoServiceApp/StateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace AutoServiceApp
+{
+    public class StateManager
+    {
+        public void SaveState(string filePath, object state)
+        {
+            var json = JsonSerializer.Serialize(state);
+            File.WriteAllText(filePath, json);
+        }
+
+        public T LoadState<T>(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
